d2bd000cf17c62a0317f795ffb37a9966c3e8ff1
Ryndhk <rayandhika@students.amikom.ac.id>
2023-03-15 12:08:21 +0700

    Aku Juga Mau

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,24 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 12;
+		int b = 5;
+		
+		Console.Write("Hasil Penambahan: {0} + {1}", a, b, Penambahan(a, b));
+		Console.Write("Hasil Pengurangan} + {1}", a, b, Penguranga (a, b));
+		
+		Console.Write("\nTekan Sembarang Key untuk keluar");
+		Console.Readkey();
 		
 	}
 	
-}
+	static int Penambahan (int a, int b)
+	{
+		return a + b;
+	}	
+	
+	static int Pengurangan (int a, int b)
+	{
+		return a - b;
+	}	
+}
